9f91a30f9a7945708e1b5f8ad549358e9563820b
Ahnming Loke <lokeahnming@gmail.com>
2019-12-06 21:36:08 -0500

    feat: Fetching data for pages lesson

--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,7 +1,8 @@
 import { useRouter } from 'next/router';
 import Layout from '../../components/MyLayout';
-
-const Page = () => {
+import fetch from 'isomorphic-unfetch';
+/**
+  const Page = () => {
   const router = useRouter();
 
   return (
@@ -11,5 +12,26 @@ const Page = () => {
     </Layout>
   );
 };
-
 export default Page;
+**/
+
+
+const Post = props => (
+  <Layout>
+    <h1>{props.show.name}</h1>
+    <p>{props.show.summary.replace(/<[/]?[pb]>/g, '')}</p>
+    <img src={props.show.image.medium}/>
+  </Layout>
+);
+
+Post.getInitialProps = async function(context) {
+  const { id } = context.query;
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  const show = await res.json();
+
+  console.log(`Fetched show: ${show.name}`);
+
+  return { show };
+}
+
+export default Post;
